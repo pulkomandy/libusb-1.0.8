0b9f22b6d2ee057835d61d79fbbe41523488cab0
Philippe Houdoin <philippe.houdoin@gmail.com>
2013-09-10 13:39:19 +0200

    Several fixes, allowing stress tests to pass: - fix broken deviceWasAllocated logic in _AddNewDevice(), fixing the   crash observed before. - start only once our UsbRoster at first context init, and stop it at last exit. - add trace to track UsbRoster's DeviceAdded/Removed() : seems that BUSBRoster:DeviceRemoved() is only called on hub nodes!?

--- libusb/os/haiku_usbkit.cpp
+++ libusb/os/haiku_usbkit.cpp
@@ -433,8 +433,6 @@ public:
 	virtual status_t    DeviceAdded(BUSBDevice* device);
 	virtual void        DeviceRemoved(BUSBDevice* device);
 
-	status_t			GetDevices(BList& list);
-
 private:
 	status_t			_AddNewDevice(struct libusb_context* ctx, UsbDeviceInfo* info);
 	
@@ -442,11 +440,10 @@ private:
 	BList	fDevices;
 };
 
+
 status_t
 UsbRoster::_AddNewDevice(struct libusb_context* ctx, UsbDeviceInfo* deviceInfo)
 {
-	bool deviceWasAllocated = false;
-	
 	struct libusb_device* dev = usbi_get_device_by_session_id(ctx, (unsigned long)deviceInfo);
 	if (dev) {
 		usbi_info (ctx, "using existing device for location ID 0x%08x", deviceInfo);
@@ -454,36 +451,36 @@ UsbRoster::_AddNewDevice(struct libusb_context* ctx, UsbDeviceInfo* deviceInfo)
 		usbi_info (ctx, "allocating new device for session ID 0x%08x", deviceInfo);
 		dev = usbi_alloc_device(ctx, (unsigned long)deviceInfo);
 		if (!dev) {
-			goto error;
+			return B_NO_MEMORY;
 		}
-		deviceWasAllocated = true;
+		*((UsbDeviceInfo**)dev->os_priv) = deviceInfo;
+
+		// TODO: handle device address mapping for devices in non-root hub(s)
+		sscanf(deviceInfo->Location(), "/%d/%d", &dev->bus_number, &dev->device_address);
+		dev->num_configurations = (uint8_t) deviceInfo->CountConfigurations();
+
+		// printf("bus %d, address %d, # of configs %d\n", dev->bus_number,
+		//	dev->device_address, dev->num_configurations);
+
+    	if(usbi_sanitize_device(dev) < 0) {
+			libusb_unref_device(dev);
+			return B_ERROR;	
+		}
 	}
 
-	*((UsbDeviceInfo**)dev->os_priv) = deviceInfo;
-
-	// TODO: handle device address mapping for devices in non-root hub(s)
-	sscanf(deviceInfo->Location(), "/%d/%d", &dev->bus_number, &dev->device_address);
-	dev->num_configurations = (uint8_t) deviceInfo->CountConfigurations();
-
-	// printf("bus %d, address %d, # of configs %d\n", dev->bus_number,
-	//	dev->device_address, dev->num_configurations);
-
-    if(usbi_sanitize_device(dev) < 0)
-   		goto error;
-
     usbi_connect_device (dev);
-
-error:
-	if (deviceWasAllocated)
-		libusb_unref_device(dev);
-	
-	return B_OK;
+   	return B_OK;
 }
 
 
 status_t
 UsbRoster::DeviceAdded(BUSBDevice* device)
 {
+#if TRACE
+	printf("UsbRoster::DeviceAdded(BUSBDevice %p: %s%s)\n", device, kBusRootPath, 
+		device->Location());
+#endif
+ 
 	if (device->IsHub())
 		// Ignore hub device(s)
 		return B_OK;
@@ -491,15 +488,11 @@ UsbRoster::DeviceAdded(BUSBDevice* device)
 	UsbDeviceInfo* deviceInfo = new UsbDeviceInfo(device);
 	deviceInfo->Get();
 	
-#if TRACE
-	printf("UsbRoster::DeviceAdded(%p: %s%s)\n", deviceInfo, kBusRootPath, 
-		deviceInfo->Location());
-#endif
- 
 	// Add this new device to each active context's device list
 	struct libusb_context *ctx;
 	usbi_mutex_lock(&active_contexts_lock);
 	list_for_each_entry(ctx, &active_contexts_list, list, struct libusb_context) {
+		// printf("UsbRoster::DeviceAdded() active_contexts_list: ctx %p\n", ctx); 
 		_AddNewDevice(ctx, deviceInfo);
 	}
 	usbi_mutex_unlock(&active_contexts_lock);
@@ -512,13 +505,21 @@ UsbRoster::DeviceAdded(BUSBDevice* device)
 void
 UsbRoster::DeviceRemoved(BUSBDevice* device)
 {
-	BAutolock locker(fDevicesLock);
+#if TRACE
+	printf("UsbRoster::DeviceRemoved(BUSBDevice %p: %s%s)\n", device, kBusRootPath, 
+		device->Location());
+#endif
 
+	if (device->IsHub())
+		// Ignore hub device(s)
+		return;
+
+	BAutolock locker(fDevicesLock);
 	UsbDeviceInfo* deviceInfo;
 	int i = 0;
 	while (deviceInfo = (UsbDeviceInfo*)fDevices.ItemAt(i++)) {
 		if (!deviceInfo)
-			break;
+			continue;
 
 		if (strcmp(deviceInfo->Location(), device->Location()) == 0)
 			break;
@@ -526,11 +527,6 @@ UsbRoster::DeviceRemoved(BUSBDevice* device)
 
 	if (!deviceInfo)
 		return;
-
-#if TRACE		
-	printf("UsbRoster::DeviceRemoved(%p: %s%s)\n", deviceInfo, 
-		kBusRootPath, deviceInfo->Location());
-#endif		
 
 	// Remove this device from each active context's device list 
 	struct libusb_context *ctx;
@@ -550,27 +546,19 @@ UsbRoster::DeviceRemoved(BUSBDevice* device)
 }
 
 
-status_t
-UsbRoster::GetDevices(BList& list)
-{
-	BAutolock locker(fDevicesLock);
-	list = fDevices;
-	return B_OK;
-}
-
-
 //  #pragma mark - libusb Haiku backend
 
 // Context
 
 
-UsbRoster gUsbRoster;
-
+UsbRoster 		gUsbRoster;
+volatile int32	gInitCount = 0;
 
 static int
 haiku_init(struct libusb_context* ctx)
 {
-	gUsbRoster.Start();
+	if (atomic_add(&gInitCount, 1) == 0)
+		gUsbRoster.Start();
 	return 0;
 }
 
@@ -578,7 +566,8 @@ haiku_init(struct libusb_context* ctx)
 static void
 haiku_exit(void)
 {
-	gUsbRoster.Stop();
+	if (atomic_add(&gInitCount, -1) == 1)
+		gUsbRoster.Stop();
 }
 
 
@@ -681,12 +670,11 @@ void
 haiku_destroy_device(struct libusb_device* dev)
 {
 	UsbDeviceInfo* deviceInfo = *((UsbDeviceInfo**)dev->os_priv);
-
-	delete deviceInfo;
+	deviceInfo->Put();
 	*((UsbDeviceInfo**)dev->os_priv) = NULL;
 
 }
-
+ 
 
 // #pragma mark -----
 
